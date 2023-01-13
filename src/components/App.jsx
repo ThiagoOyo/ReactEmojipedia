e8448795121f5b2612ddf593db80e5ecd4707df3
ThiagoOyo <107907441+ThiagoOyo@users.noreply.github.com>
2023-01-13 16:15:45 -0300

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import emojipedia from "../emojipedia"
+import Entry from "./Entry"
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map(entry =>  (<Entry  key={entry.id} id={entry.id} emoji={entry.emoji} name={entry.name} meaning={entry.meaning}/>))}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
